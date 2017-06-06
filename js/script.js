7ea0360c2290b7557cb0932b9f2e99143285d57e
Steve Zaske <github@zaske.com>
2017-06-06 11:53:46 -0700

    add business logic and results

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
-$(document).ready(function() {
+//Business Logic
+var goodLuck = 4;
+var badLuck = 0;
 
-    // You're code goes here
+function calcFortune(omen) {
+  if (omen === "good") goodLuck++
+  else badLuck++;
+}
 
+function findFortune(luck){
+  results = ["an uber fail whale of unluckiness.","very unlucky.","sort of unlucky.","unlucky.","neither.","lucky.","very lucky.","extremely luck.","at Brad Pitt's level of luck."];
+  return results[luck];
+  }
+
+
+//UI Logic
+$(document).ready(function(){
+
+  //create a form submission event listener
+  $("#fortuneForm").submit(function(event){
+    event.preventDefault();
+    $("#results").show(); // show the results
+
+    //Write out the results in HTML
+    $('#results').append("Because of your history of with...");
+    $("input:checkbox[name=Omen]:checked").each(function(){
+      var Omens = $(this).val();
+      $('#results').append(Omens + ", ");
+      calcFortune($(this).attr('class'));
+    });
+
+    $('#results').append("I consider you...");
+
+    // determine the fortune
+    $('#results').append(findFortune(goodLuck-badLuck));
+    console.log("goodluck=",goodLuck, " badluck = ", badLuck);
+  });
 });
